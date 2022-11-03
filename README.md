63af38fd79d5944b3f2c7d57bab2a50c881f3f21
ridowayne <hassanrilwan12@gmail.com>
2022-11-03 07:39:27 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # hngCsvconversionTask
+
 converting a csv file to json, calculating the sha256 of the json file and append it to each line in the csv
+
+the code converts the Team Engine file to json then calculate the sha256 of the csv file and then converts it back to csv file
+run 'node app.js'
---
+++ app.js
@@ -0,0 +1,26 @@
+const csvtojson = require("csvtojson");
+const json2csv = require("json2csv").parse;
+const crypto = require("crypto");
+
+const fs = require("fs");
+
+csvtojson()
+  .fromFile("Naming - Team Engine.csv")
+  .then((sourcefile) => {
+    for (let i = 0; i < sourcefile.length; i++) {
+      const hash = crypto
+        .createHash("sha256")
+        .update(i.toString())
+        .digest("hex");
+      sourcefile[i].HASH = hash;
+    }
+    const output = JSON.stringify(sourcefile);
+
+    fs.writeFileSync("./nftTeamEngine.json", output);
+
+    console.log(sourcefile);
+    const csv = json2csv(sourcefile, {
+      fields: ["Series Number", "FILE NAME", "UUID", "Description", "HASH"],
+    });
+    fs.writeFileSync("./finalnft.csv", csv);
+  });
