4f8559202287d9f1377707ea06f87ddf2b87062c
ridowayne <hassanrilwan12@gmail.com>
2022-11-04 09:37:07 +0100

    another commit

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # hngCsvconversionTask
 
 # Notice
+
 Update has been made to CSV file to create structured and well formatted data for transformation and processing.
 
 In the case of the attribute values, each pair is delimited with a ; else using a comman "," will create a complex data set that is difficult to process and transform;
@@ -14,6 +15,10 @@ the file to be converted should be in the same folder for easy access
 the code converts the Team Engine file to json then calculate the sha256 of the csv file and then converts it back to csv file
 
 # comand
+
 After cloning and install all necessary dependencies by running "npm install" in your terminal,
 
-run 'node app.js' and the filename that you want to convert
+eit the app.js file and input the filename you want to convert
+edit the placeholder to put the output file name
+
+run 'node app.js'
--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const json2csv = require("json2csv").parse;
 const crypto = require("crypto");
 
 const fs = require("fs");
-const filePath = process.argv[1] || "HNGi9 CSV FILE - Sheet1.csv";
+const filePath = process.argv[1] || "HNGi9CSVFILE";
 
 csvtojson()
-  .fromFile(filePath)
+  .fromFile("Your file path")
   .then((sourcefile) => {
     for (let i = 0; i < sourcefile.length; i++) {
       const hash = crypto
@@ -14,14 +14,16 @@ csvtojson()
         .update(i.toString())
         .digest("hex");
       sourcefile[i].Hash = hash;
+      sourcefile[i].format = "CHIP-0007";
     }
-    const output = JSON.stringify(sourcefile);
+    // const output = JSON.stringify(sourcefile);
 
-    fs.writeFileSync("./nftAllTeams.json", output);
+    // fs.writeFileSync("./nftAllTeams.json", output);
 
     console.log(sourcefile);
     const csv = json2csv(sourcefile, {
       fields: [
+        "format",
         "TEAM NAMES",
         "Series Number",
         "Filename",
@@ -33,6 +35,6 @@ csvtojson()
         "Hash",
       ],
     });
-    fs.writeFileSync(`${filePath.split(".")[0]}.output.csv`, csv);
+    fs.writeFileSync(`The-new csv-file.output.csv`, csv);
     console.log(`output generated successfully`);
   });
