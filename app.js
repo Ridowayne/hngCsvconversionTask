95626f9b4b9ec551a0204393c455a3664e0f7e7d
ridowayne <hassanrilwan12@gmail.com>
2022-11-04 08:23:43 +0100

    another commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const json2csv = require("json2csv").parse;
 const crypto = require("crypto");
 
 const fs = require("fs");
-const filePath = process.argv[1] || "Naming - Team Engine.csv";
+const filePath = process.argv[1] || "HNGi9 CSV FILE - Sheet1.csv";
 
 csvtojson()
   .fromFile(filePath)
@@ -13,15 +13,25 @@ csvtojson()
         .createHash("sha256")
         .update(i.toString())
         .digest("hex");
-      sourcefile[i].HASH = hash;
+      sourcefile[i].Hash = hash;
     }
     const output = JSON.stringify(sourcefile);
 
-    fs.writeFileSync("./nftTeamEngine.json", output);
+    fs.writeFileSync("./nftAllTeams.json", output);
 
     console.log(sourcefile);
     const csv = json2csv(sourcefile, {
-      fields: ["Series Number", "FILE NAME", "UUID", "Description", "HASH"],
+      fields: [
+        "TEAM NAMES",
+        "Series Number",
+        "Filename",
+        "Name",
+        "Description",
+        "Gender",
+        "Attributes",
+        "UUID",
+        "Hash",
+      ],
     });
     fs.writeFileSync(`${filePath.split(".")[0]}.output.csv`, csv);
     console.log(`output generated successfully`);
